0d92e02e50f1f1adbb91fcf5c6ae7c8a1eb75252
pwvspw <pengwei@dztec.net>
2022-08-31 11:39:15 +0800

    add redisson

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyRedissonConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.product.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyRedissonConfig {
+
+    /**
+     * 所有对Redisson的使用都使用RedissonClient对象
+     *
+     * destroyMethod = "shutdown"：配置销毁方式
+     */
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+
+
+}
